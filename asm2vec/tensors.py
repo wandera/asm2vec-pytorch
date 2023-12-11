d93fe7676d0a21354d78d5b4ae6f479c62e9c61e
ilektragiassa <117294049+ilektragiassa@users.noreply.github.com>
2023-12-11 12:52:27 +0100

    AEGIS-6871 split function for saving partial tensors

--- asm2vec/tensors.py
+++ asm2vec/tensors.py
@@ -12,14 +12,14 @@ logging.basicConfig(level=logging.INFO, format='%(message)s')
 def move_files(abs_dirname, limit):
     """Moves files from a directory into subdirectories
     :param abs_dirname: absolute path to the directory
-    :param limit: if the directory contains more that #limit files, they are split into subdirectories"""
+    :param limit: if the directory contains more that #limit files, they are split into subdirectories
+    """
     files = [os.path.join(abs_dirname, f) for f in os.listdir(abs_dirname) if not f.startswith('.')]
     if len(files) > limit:
         count = 0
         for f in files:
             if count % limit == 0:
                 subdir_name = f'{abs_dirname}_{count // limit}'
-                print(subdir_name)
                 os.mkdir(subdir_name)
 
             f_base = os.path.basename(f)
@@ -27,7 +27,25 @@ def move_files(abs_dirname, limit):
             count += 1
 
 
-def calc_tensors(asm_path: str, tensor_path: str, model_path: str, epochs: int, limit: int,
+def save_partial_tensors(tensor_path: str):
+    """Saves the tensor produced per binary that exceeds the limit of assembly functions as a single tensor
+    by calculating the average of the partial tensors
+    :param tensor_path: path to the directory where the tensors are saved
+    """
+    tensors_full_list = [f[:40] for f in os.listdir(tensor_path) if (not f.startswith('.'))]
+    tensors_list = list(set(tensors_full_list))
+
+    for tensor in tensors_list:
+        if tensors_full_list.count(tensor) > 1:
+            tensor_list = [torch.load(Path(os.path.join(tensor_path, x))).detach().squeeze() for x in
+                           os.listdir(tensor_path) if x[:40] == tensor]
+            torch.save(torch.mean(torch.stack(tensor_list), dim=0), os.path.join(tensor_path, tensor))
+            for fname in os.listdir(tensor_path):
+                if fname.startswith(f"{tensor}_"):
+                    os.remove(os.path.join(tensor_path, fname))
+
+
+def calc_tensors(asm_path: str, tensor_path: str, model_path: str, epochs: int, limit: int = 300,
                  device: str = 'cpu', learning_rate: float = 0.02) -> list:
     """
     Calculates vector representation of a binary as the mean per column of the vector representations of its assembly
@@ -48,7 +66,7 @@ def calc_tensors(asm_path: str, tensor_path: str, model_path: str, epochs: int, 
     if os.path.isfile(model_path):
         model, tokens = load_model(model_path, device=device)
     else:
-        print("No valid model")
+        logging.info("No valid model")
         return []
 
     dir0 = Path(tensor_path)
@@ -92,16 +110,18 @@ def calc_tensors(asm_path: str, tensor_path: str, model_path: str, epochs: int, 
     else:
         logging.info("No valid directory")
 
-    tensors_full_list = [f[:40] for f in os.listdir(tensor_path) if (not f.startswith('.'))]
-    tensors_list = list(set(tensors_full_list))
+    save_partial_tensors(tensor_path)
 
-    for tensor in tensors_list:
-        if tensors_full_list.count(tensor) > 1:
-            tensor_list = [torch.load(Path(os.path.join(tensor_path, x))).detach().squeeze() for x in
-                           os.listdir(tensor_path) if x[:40] == tensor]
-            torch.save(torch.mean(torch.stack(tensor_list), dim=0), os.path.join(tensor_path, tensor))
-            for fname in os.listdir(tensor_path):
-                if fname.startswith(f"{tensor}_"):
-                    os.remove(os.path.join(tensor_path, fname))
+    # tensors_full_list = [f[:40] for f in os.listdir(tensor_path) if (not f.startswith('.'))]
+    # tensors_list = list(set(tensors_full_list))
+    #
+    # for tensor in tensors_list:
+    #     if tensors_full_list.count(tensor) > 1:
+    #         tensor_list = [torch.load(Path(os.path.join(tensor_path, x))).detach().squeeze() for x in
+    #                        os.listdir(tensor_path) if x[:40] == tensor]
+    #         torch.save(torch.mean(torch.stack(tensor_list), dim=0), os.path.join(tensor_path, tensor))
+    #         for fname in os.listdir(tensor_path):
+    #             if fname.startswith(f"{tensor}_"):
+    #                 os.remove(os.path.join(tensor_path, fname))
 
     return tensors_list
