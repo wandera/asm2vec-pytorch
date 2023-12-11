2e0fdcdbdc5404f056432794e9b682e33eca1ac6
ilektragiassa <117294049+ilektragiassa@users.noreply.github.com>
2023-12-11 15:24:26 +0100

    AEGIS-6871 Update tensors.py

--- asm2vec/tensors.py
+++ asm2vec/tensors.py
@@ -49,7 +49,7 @@ def save_partial_tensors(tensor_path: str) -> list:
     return tensors_final_list
 
 
-def calc_tensors(asm_path: str, tensor_path: str, model_path: str, epochs: int, limit: int = 300,
+def calc_tensors(asm_path: str, tensor_path: str, model_path: str, epochs: int, limit: int,
                  device: str = 'cpu', learning_rate: float = 0.02) -> list:
     """
     Calculates vector representation of a binary as the mean per column of the vector representations of its assembly
